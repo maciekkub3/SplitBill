6d4af3bf7f7c065f30261d1c9c4116c1295c6160
maciekkub3 <78180808+maciekkub3@users.noreply.github.com>
2025-07-16 15:09:45 +0200

    feature/navigation
    * initial navigation setup
    
    * initial navigation setup

---
+++ app/src/main/java/com/example/splitbill/navigation/Navigation.kt
@@ -0,0 +1,27 @@
+package com.example.splitbill.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+@Composable
+fun Navigation(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoute.route
+    ) {
+        composable<HomeRoute> {
+            // HomeScreen(navController = navController, state = state)
+        }
+        composable<AddEventRoute> {
+            // AddEventScreen(navController = navController, state = state)
+        }
+        composable<EventRoute> {
+            // EventScreen(navController = navController, state = state)
+        }
+    }
+}
