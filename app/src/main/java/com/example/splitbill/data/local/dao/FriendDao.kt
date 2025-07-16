fc46b5663e873603b6279337baa63c34b25c313a
maciekkub3 <maciek.k2001@gmail.com>
2025-07-16 13:52:05 +0200

    initial room setup

--- app/src/main/java/com/example/splitbill/data/local/dao/FriendDao.kt
+++ app/src/main/java/com/example/splitbill/data/local/dao/FriendDao.kt
@@ -2,7 +2,6 @@ package com.example.splitbill.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Upsert
 import com.example.splitbill.data.local.entity.Friend
