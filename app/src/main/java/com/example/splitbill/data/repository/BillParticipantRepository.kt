4582a5a1599cdd349f1d4ebe1cde8ee633ddc0f1
maciekkub3 <78180808+maciekkub3@users.noreply.github.com>
2025-07-17 12:55:59 +0200

    initial repostiory setup (#5)

---
+++ app/src/main/java/com/example/splitbill/data/repository/BillParticipantRepository.kt
@@ -0,0 +1,18 @@
+package com.example.splitbill.data.repository
+
+
+import com.example.splitbill.data.local.dao.BillParticipantDao
+import com.example.splitbill.data.local.entity.BillParticipant
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class BillParticipantRepository @Inject constructor(
+    private val billParticipantDao: BillParticipantDao
+) {
+
+    fun getParticipantsForBill(billId: Long): Flow<List<BillParticipant>> = billParticipantDao.getParticipantsForBill(billId)
+
+    suspend fun upsertBillParticipant(participant: BillParticipant) = billParticipantDao.upsertBillParticipant(participant)
+
+    suspend fun deleteBillParticipant(participant: BillParticipant) = billParticipantDao.deleteBillParticipant(participant)
+}
